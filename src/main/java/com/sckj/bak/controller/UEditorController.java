2b2a6b326da5b455ca342bcb0012acbef0596c43
huangweiwei <824565636@qq.com>
2018-08-31 18:26:40 +0800

    文件移动

--- src/main/java/com/sckj/bak/controller/UEditorController.java
+++ src/main/java/com/sckj/bak/controller/UEditorController.java
@@ -1,21 +1,124 @@
 package com.sckj.bak.controller;
 
-import com.sckj.utils.FileUtils;
+
+import com.alibaba.fastjson.JSON;
+import com.baidu.ueditor.ActionEnter;
+import com.sckj.dto.Ueditor;
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.UUID;
 
 
 @RestController
-@RequestMapping("shoucha/frame/ueditor")
+//@RequestMapping("shoucha/frame/ueditor")
 public class UEditorController {
+
+    @RequestMapping(value={"/shoucha/frame/ueditor/ueditorConfig","/ueditorConfig"})
+    @ResponseBody
+    public String ueditor(@RequestParam("action") String param,MultipartFile upfile,HttpServletRequest request) {
+        Ueditor ueditor = new Ueditor();
+        if(param!=null&&param.equals("config")){
+            //return PublicMsg.UEDITOR_CONFIG;
+            String rootPath = "src/main/resources/static";
+            if(request.getRequestURI().equals("/shoucha/frame/ueditor/ueditorConfig")){
+                rootPath = "src/main/webapp";
+            }
+
+            String exec = new ActionEnter(request, rootPath).exec();
+            return exec;
+        }else if(param!=null&&param.equals("uploadimage")||param.equals("uploadscrawl")){
+            if(upfile!=null){
+                //{state：”数据状态信息”，url：”图片回显路径”，title：”文件title”，original：”文件名称”，···}
+                try {
+                    return uploadImg(upfile,request);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    ueditor.setState("出现异常");
+                    return JSON.toJSONString(ueditor);
+                }
+            }else{
+                ueditor.setState("文件为空");
+                return JSON.toJSONString(ueditor);
+            }
+        }else{
+            ueditor.setState("不支持该操作");
+            return JSON.toJSONString(ueditor);
+        }
+    }
+
+    @RequestMapping(value="/imgUpload")
+    @ResponseBody
+    public Ueditor imgUpload(@RequestParam("action") String param,MultipartFile upfile,HttpServletRequest request) {
+        Ueditor ueditor = new Ueditor();
+        return ueditor;
+    }
+
+    public String uploadImg(MultipartFile file,
+                            HttpServletRequest request) throws IOException {
+        Ueditor ueditor = new Ueditor();
+//        Users user = UserUtils.getUser(request) ;
+        String path = request.getSession().getServletContext()
+                .getRealPath("ueditor/jsp/upload/image");
+        String ct = file.getContentType() ;
+        String fileType = "";
+        if (ct.indexOf("/")>0) {
+            fileType = ct.substring(ct.indexOf("/")+1);
+        }
+        String fileName = UUID.randomUUID() + "." + fileType;
+        File targetFile = new File(path);
+        if (!targetFile.exists()) {
+            targetFile.mkdirs();
+        }
+        File targetFile2 = new File(path+"/"+fileName);
+        if (!targetFile2.exists()) {
+            targetFile2.createNewFile();
+        }
+        // 保存
+        try {
+            file.transferTo(targetFile2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ueditor.setState("SUCCESS");
+        ueditor.setTitle(fileName);
+        ueditor.setOriginal(fileName);
+        ueditor.setUrl("/ueditor/jsp/upload/image"+File.separator+fileName);
+        System.out.println( JSON.toJSONString(ueditor));
+        return JSON.toJSONString(ueditor);
+    }
+
+//======================================================================================================================================================
+
+    @Autowired
+    private HttpServletRequest request;
+    @RequestMapping("/ueditorConfig_")
+    public void getUEditorConfig(HttpServletResponse response){
+        String rootPath = "src/main/resources/static";
+        try {
+            String exec = new ActionEnter(request, rootPath).exec();
+            PrintWriter writer = response.getWriter();
+            writer.write(exec);
+            writer.flush();
+            writer.close();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     @RequestMapping(value = "/jsp/config",headers = "Accept=application/json")
     public String imgUpload(HttpServletRequest request,HttpServletResponse response) {
         response.setContentType("application/json;charset=utf-8");

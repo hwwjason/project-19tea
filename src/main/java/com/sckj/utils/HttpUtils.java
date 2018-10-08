9ecd9f427e6c97ac786a84076b523a8a32528616
huangweiwei <824565636@qq.com>
2018-10-08 19:39:53 +0800

    测试

--- src/main/java/com/sckj/utils/HttpUtils.java
+++ src/main/java/com/sckj/utils/HttpUtils.java
@@ -43,6 +43,7 @@ public class HttpUtils {
             InetAddress ip = null;
             while (netInterfaces.hasMoreElements()) {
                 NetworkInterface ni = (NetworkInterface) netInterfaces.nextElement();
+                System.out.println(ni.getName());
                 if(!"en0".equals(ni.getName()) && !"Ethernet0".equals(ni.getName()) ){
                     continue;
                 }

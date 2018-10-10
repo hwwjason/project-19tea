ecd99071a8622021c86468d85683eed0e7bbe666
linzhengyuan <492883577@qq.com>
2018-10-10 17:41:40 +0800

    上传地址修改

--- src/main/resources/static/shoucha/frame/webuploader/upload.js
+++ src/main/resources/static/shoucha/frame/webuploader/upload.js
@@ -151,7 +151,7 @@
                     swf: '../../dist/Uploader.swf',
                     chunked: false,
                     chunkSize: 512 * 1024,
-                    server: baseURL+'/bak/image/uploadImage',
+                    server: 'https://www.sowtea.com:2433/bak/image/uploadImage',
                     // runtimeOrder: 'flash',
 
                     // accept: {
